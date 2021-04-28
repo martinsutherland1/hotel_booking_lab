1de312136a8b86d061d596ccf62bd4ae18d025d5
pooja maheshwari <pooja.chugh15@gmail.com>
2021-04-28 16:59:35 +0100

    mvp done

--- client/src/components/BookingList.js
+++ client/src/components/BookingList.js
@@ -1,21 +1,31 @@
 import React from "react";
 import BookingInfo from "./BookingInfo";
+import { deleteBookings } from "../container/Bookings_service";
 
-const BookingList = ({bookingList, onBookingClick, selectedBooking}) => {
+const BookingList = ({bookingList, onBookingClick, selectedBooking, removeBooking}) => {
+    
     
     const handleClick = () => {
       onBookingClick(selectedBooking)
     }
+    const handleDelete = (e) => {
+      deleteBookings(e.target.value).then(() => {
+        removeBooking(e.target.value)
+      })
+    }
+    // deleteSighting(sighting._id).then(()=>{
+    //   removeSighting(sighting._id);
 
     return (
         <div>
         <h1>booking list</h1>
         <ul>
         {bookingList.map((booking, index) => {
-          return <li onClick={handleClick} key={index}>{booking.name}</li>
+          return <li onClick={handleClick} key={index}>{booking.name} <button value={booking._id} onClick={handleDelete}> Delete</button></li>
         })}
         </ul>
         <BookingInfo selectedBooking={selectedBooking}/>
+        
         </div>
     )
 }
--- client/src/container/HotelBookingApp.js
+++ client/src/container/HotelBookingApp.js
@@ -21,12 +21,29 @@ const HotelBookingApp = () => {
       setSelectedBooking(selectedBooking)
     }
 
+    const removeBooking = (id) => {
+      const temp = bookingList.map(booking => booking);
+      const indexToDel = temp.map(booking => booking._id).indexOf(id);
+      console.log(indexToDel);
+      temp.splice(indexToDel, 1);
+     setBookingList(temp);
+    }
+
+    // const removeSighting = (id) => {
+    //   const temp = birdSightings.map(s =>s);
+    //   const indexToDel = temp.map(s => s._id).indexOf(id);
+    //   console.log(indexToDel);
+  
+    //   temp.splice(indexToDel, 1);
+    //   setBirdSightings(temp);
+    // }
+
 
 
     return (
         <div>
         <h1>Last Hope Hotel</h1>
-        <BookingList bookingList={bookingList} selectedBooking={selectedBooking} onBookingClick={onBookingClick}/>
+        <BookingList bookingList={bookingList} selectedBooking={selectedBooking} onBookingClick={onBookingClick} removeBooking={removeBooking}/>
         <BookingForm addBooking={addBooking}/>
         </div>
     )
